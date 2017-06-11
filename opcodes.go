8aa53c23cbb6e64ca1e7c27be74fb062ade5932d
Jeromy <jeromyj@gmail.com>
2017-06-11 10:13:39 -0700

    better address extraction

--- opcodes.go
+++ opcodes.go
@@ -10,6 +10,7 @@ const (
 
 	OP_DUP = 0x76
 
+	OP_EQUAL       = 0x87
 	OP_EQUALVERIFY = 0x88
 
 	OP_CHECKSIG = 0xac
@@ -32,15 +33,43 @@ func PayToPubkeyScript(addr string) []byte {
 	return append(append([]byte{OP_DUP, OP_HASH160, byte(len(pkval))}, pkval...), OP_EQUALVERIFY, OP_CHECKSIG)
 }
 
-func PayToPubkeyAddr(script []byte) ([]byte, error) {
+func PayToPubkeyAddr(script []byte) []byte {
 	if len(script) < 6 {
-		return nil, nil
+		return nil
 	}
 	if script[0] != OP_DUP || script[1] != OP_HASH160 || script[len(script)-1] != OP_CHECKSIG || script[len(script)-2] != OP_EQUALVERIFY {
-		return nil, nil
+		return nil
 	}
 
-	return script[3 : len(script)-2], nil
+	return script[3 : len(script)-2]
+}
+
+func PayToScriptHashAddr(script []byte) []byte {
+	if len(script) < 4 || script[0] != OP_HASH160 || script[len(script)-1] != OP_EQUAL {
+		return nil
+	}
+
+	return script[2 : len(script)-1]
+}
+
+const (
+	AddressTypeNonStandard = 0
+	AddressTypeP2PKH       = 1
+	AddressTypeP2SH        = 2
+)
+
+func ExtractAddressFromScript(script []byte) ([]byte, int, error) {
+	a := PayToPubkeyAddr(script)
+	if a != nil {
+		return a, AddressTypeP2PKH, nil
+	}
+
+	a = PayToScriptHashAddr(script)
+	if a != nil {
+		return a, AddressTypeP2SH, nil
+	}
+
+	return nil, 0, nil
 }
 
 func MakeMultisig(m, n int, pubks [][]byte) []byte {
